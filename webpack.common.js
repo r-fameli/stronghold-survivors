c8bf7e9e8a1f9e9e94e48c485634306e8f7d6351
r-fameli <rikifameli@gmail.com>
2026-05-02 16:38:58 -0700

    reinstall dependencies

--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,17 @@
-// header: webpack.common.js
+// Learn more about this file at:
+// https://victorzhou.com/blog/build-an-io-game-part-1/#2-builds--project-setup
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    game: './src/client/index.js',  
-},
+    game: './src/client/index.js',
+  },
   output: {
     filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),  
-},
+    path: path.resolve(__dirname, 'dist'),
+  },
   module: {
     rules: [
       {
@@ -18,8 +20,8 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env'],          
-},
+            presets: ['@babel/preset-env'],
+          },
         },
       },
       {
@@ -35,8 +37,8 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css',    
-}),
+      filename: '[name].[contenthash].css',
+    }),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'src/client/html/index.html',
