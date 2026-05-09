6ca1bea7f7ce75c2367f93cfb2dcf557ba5bb921
r-fameli <rikifameli@gmail.com>
2026-05-08 18:34:30 -0700

    Update player speed to initialize as 0

--- src/server/player.js
+++ src/server/player.js
@@ -4,7 +4,7 @@ const Constants = require('../shared/constants');
 
 class Player extends ObjectClass {
   constructor(id, username, x, y) {
-    super(id, x, y, Math.random() * 2 * Math.PI, Constants.PLAYER_SPEED);
+    super(id, x, y, Math.random() * 2 * Math.PI, 0);
     this.username = username;
     this.hp = Constants.PLAYER_MAX_HP;
     this.fireCooldown = 0;
